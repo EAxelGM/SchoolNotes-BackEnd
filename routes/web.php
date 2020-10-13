290ea261e5c5ce3aea5d3befc228320780aa54ec
EAxelGM <axel@excite.com.mx>
2020-10-13 01:39:38 -0500

    correcion de rutas protegidas

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,14 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     /*AÑADE AQUI LAS RUTAS QUE QUIERAS PROTEGER CON JWT*/
     Route::group(['namespace' => 'Api'], function() {
         Route::get('/layout-email', 'UserController@index')->name('layout');
-        Route::get('/validar/{id}/{token}', 'UserController@validarCorreo')->name('validarCorreo');
-        Route::get('/recuperar-contrasena/{id}/{token}', 'UserController@recuperarPassword')->name('recuperarContrasena');
     });
+    
 });
 
-//Auth::routes();
+Route::group(['namespace' => 'Api'], function() {
+    Route::get('/validar/{id}/{token}', 'UserController@validarCorreo')->name('validarCorreo');
+    Route::get('/recuperar-contrasena/{id}/{token}', 'UserController@recuperarPassword')->name('recuperarContrasena');
+});
+    //Auth::routes();
 //Route::get('/home', 'HomeController@index')->name('home');
 
