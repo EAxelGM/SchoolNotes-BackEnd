92828e483c154429434b1942b4335aaa292e0ec6
root <root@schoolnotes.live>
2020-10-13 09:30:36 +0000

    Desproteccion de algunas rutas

--- routes/api.php
+++ routes/api.php
@@ -17,66 +17,64 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::group(['namespace' => 'Api'], function() {
-    
+
     //Login y Register para JWT
     Route::post('register', 'UserController@register');
     Route::post('login', 'UserController@authenticate');
     Route::get('profile', 'UserController@getAuthenticatedUser');
     Route::post('loggout', 'UserController@loggout');
+
+    /**ETIQUETAS */
+    Route::resource('etiquetas', 'EtiquetaController');
 });
 
 Route::group(['middleware' => ['jwt.verify']], function() {
     /*AÑADE AQUI LAS RUTAS QUE QUIERAS PROTEGER CON JWT*/
     Route::group(['namespace' => 'Api'], function() {
         // Rutas de los controladores dentro del Namespace "App\Http\Controllers\Api"
-        
+
         /** USUARIOS */
         Route::resource('usuarios', 'UserController');
         Route::post('cambiar-contrasena', 'UserController@cambiarContrasena');
         Route::post('usuario-img-perfil', 'UserController@imgPerfil');
         Route::get('re-enviar-correo-verificacion/{id}', 'UserController@enviarCorreo');
-        
+
         /**pUBLICACIONES */
         Route::resource('publicaciones', 'PublicacionController');
-        
+
+        /**PREGUNTAS */
+        Route::resource('preguntas', 'PreguntaController');
+
         /**APUNTES */
         Route::resource('apuntes', 'ApunteController');
         Route::get('apuntes/{id}/usuario', 'ApunteController@apuntesUsuario');
         Route::get('apuntes/{id}/documentos-guardados', 'ApunteController@apuntesGuardados');
         Route::post('upload-file', 'ApunteController@uploadFile');
         Route::post('comprar-apunte', 'ApunteController@comprarApunte');
-        
-        /**ETIQUETAS */
-        Route::resource('etiquetas', 'EtiquetaController');
-    
+
         /**COMENTARIOS */
         Route::resource('comentarios', 'ComentarioController');
-    
-        /**PREGUNTAS */
-        Route::resource('preguntas', 'PreguntaController');
-    
+
         /**RESPUESTAS */
         Route::resource('respuestas', 'RespuestaController');
-    
+
         /**SEGUIMIENTOS */
         Route::post('seguir', 'SeguirController@seguir');
-    
+
         /**REACCIONES */
         Route::post('reaccion', 'ReaccionController@index');
-    
+
         /**RESPUESTAS */
         Route::post('valida-respuesta', 'RespuestaController@validaRespuesta');
-    
+
         /**CLIPS */
         Route::post('comprar-clips', 'ClipsController@compraClips');
-    
+
         /**WARNINGS */
         Route::resource('warning', 'WarningController');
-        
+
     });
 });
 
 
 Route::resource('categorias', 'Api\CategoriaController');
-
-
