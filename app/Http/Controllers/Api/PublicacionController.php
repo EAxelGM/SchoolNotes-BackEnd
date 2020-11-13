c5f04c094ed076ce07835a0b1bbe3c23212c8888
EAxelGM <axel@excite.com.mx>
2020-11-13 00:00:44 -0600

    correcciones en apuntes

--- app/Http/Controllers/Api/PublicacionController.php
+++ app/Http/Controllers/Api/PublicacionController.php
@@ -28,6 +28,7 @@ class PublicacionController extends Controller
         
         //$user = User::find($id);
         $user = Auth::user();
+        return response()->json(['data' => $user],400);
         if(!$user){
             return response()->json([
                 'message' => 'Este ID '. $id .' no existe',
