e9f3ceb3bee18aac25630d8bb4ecc702cde12aac
EAxelGM <axel@excite.com.mx>
2020-11-12 23:56:21 -0600

    correcciones en apuntes

--- app/Http/Controllers/Api/ApunteController.php
+++ app/Http/Controllers/Api/ApunteController.php
@@ -21,7 +21,6 @@ class ApunteController extends Controller
     public function index()
     {
         //$id = isset($_GET['user_id']);
-        $id = Auth::user()->id;
         $page = isset($_GET['page']);
         if(!$page){
             return response()->json([
@@ -31,7 +30,8 @@ class ApunteController extends Controller
         //$id = $_GET['user_id'];
         $page = $_GET['page'];
 
-        $user = User::find($id);
+        //$user = User::find($id);
+        $user = Auth::user();
         if(!$user){
             return response()->json([
                 'message' => 'Este ID '. $id .' no existe',
